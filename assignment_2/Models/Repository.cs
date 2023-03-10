ade54b55c8854c385a5318a7e9fb48c8451b1123
tarun <tarun@TARUNA>
2023-03-10 02:21:56 -0500

    Add project files.

---
+++ assignment_2/Models/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+namespace assignment_2.Models
+{
+    
+        public static class Repository { 
+
+private static List<Employee> allEmpoyees = new List<Employee> ();
+public static IEnumerable<Employee> AllEmpoyees
+        {
+            get { return allEmpoyees; }
+        }
+    public static void Create(Employee employee) { 
+    allEmpoyees.Add(employee);
+    }
+        public static void Delete(Employee employee)
+        {
+            allEmpoyees.Remove(employee);
+        }
+        public static void Update(Employee employee) { allEmpoyees.Add(employee); }
+    }
+}
